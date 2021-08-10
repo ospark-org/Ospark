e771be0bfbb96f8d1c08a97a38ba6a1d745435d6
蔡東棋 <awoo.abnertsai@gmail.com>
2021-08-11 01:49:47 +0800

    [UPDATE] modify former structure
    The new file form.py is from the original __init__.py
    Abstract part of transformer.py code to former.py

--- ospark/former/__init__.py
+++ ospark/former/__init__.py
@@ -1,20 +1,2 @@
-from ospark.nn.component.basic_module import BasicModule
-from abc import abstractmethod
-from typing import NoReturn
-import tensorflow as tf
-
-
-class Former(BasicModule):
-
-    def __init__(self, obj_name: str) -> NoReturn:
-        super().__init__(obj_name=obj_name)
-
-    def model(self, input_data: tf.Tensor) -> tf.Tensor:
-        raise NotImplementedError()
-
-    def create(self) -> NoReturn:
-        super().create("model")
-        
-    @tf.function
-    def __call__(self, input_data: tf.Tensor) -> tf.Tensor:
-        return self.model(input_data)
+from .transformer import Transformer
+from .informer import Informer
--- ospark/former/transformer.py
+++ ospark/former/transformer.py
@@ -1,136 +1,75 @@
-from . import Former
+from ospark.former.former import Former
 from ospark.nn.component.basic_module import BasicModule
 from ospark.nn.component.normalization import Normalization
-from ospark.nn.block.transformer_block import transformer
-from ospark.nn.component.weight import Weight
-from typing import List, Tuple
-from enum import Enum
-import tensorflow as tf 
-import numpy as np
+from ospark.nn.block.transformer_block import encoder_block, decoder_block
+from typing import List, NoReturn
+import tensorflow as tf
 import ospark
 
-class FormerOption(Enum):
-    
-    transformer = lambda model_name, encoder_number, embedding_size, scale_rate, head_number, class_number: \
-                   set_encoder(transformer)(model_name, encoder_number, embedding_size, scale_rate, head_number, class_number)
-
-def set_encoder(encoder):
-    def create_model(model_name, encoder_number, embedding_size, scale_rate, head_number, class_number): # TODO
-        encoder_blocks = []
-        for i in range(encoder_number):
-            obj_name = f"block_{i}"
-            encoder_blocks.append(encoder(obj_name=obj_name, embedding_size=embedding_size, scale_rate=scale_rate, head_number=head_number))
-        return Transformer(model_name, encoder_blocks, class_number, embedding_size)
-    return create_model
 
 class Transformer(Former):
  
-    def __init__(self, 
+    def __init__(self,
                  obj_name: str,
                  encoder_blocks: List[BasicModule], 
                  class_number: int, 
                  embedding_size: int,
-                 decoder_blocks: List[BasicModule]=None,
+                 decoder_blocks: List[BasicModule]=[],
                  max_length: int=2000,
                  normalization: Normalization=None, 
-                 initial_norm: bool=True):
-        super().__init__(obj_name=obj_name)
-        self._embedding_size        = embedding_size
-        self._max_length            = max_length
-        self._initial_norm          = initial_norm
-        self._encoding_table        = self.create_encoding_table()
-        self._encoder_blocks        = encoder_blocks
-        self._decoder_blocks        = decoder_blocks
-        self._class_number          = class_number
-        self._normalization         = normalization or ospark.normalization.LayerNormalization()
-        self._classify_layer        = ospark.weight.truncated_normal(obj_name="classify_layer",
-                                                                     weight_shape=[self.embedding_size, class_number])
-        self._classifier            = tf.nn.sigmoid if class_number == 2 else tf.nn.softmax
-        for component in [*self.encoder_blocks, self.classify_layer]:
-            self.assign(component)
-        if decoder_blocks is not None:
-            for component in decoder_blocks:
-                self.assign(component)
-
-    @property
-    def normalization(self) -> Normalization:
-        return self._normalization
-
-    @property
-    def embedding_size(self) -> int:
-        return self._embedding_size
-
-    @property
-    def max_length(self) -> int:
-        return self._max_length
+                 initial_norm: bool=False) -> NoReturn:
+        super().__init__(obj_name=obj_name,
+                         encoder_blocks=encoder_blocks,
+                         class_number=class_number,
+                         embedding_size=embedding_size,
+                         decoder_blocks=decoder_blocks,
+                         max_length=max_length
+                         )
+        self._normalization = normalization or ospark.normalization.LayerNormalization()
+        self._initial_norm  = initial_norm
 
     @property
     def initial_norm(self) -> bool:
         return self._initial_norm
 
     @property
-    def encoding_table(self) -> tf.Tensor:
-        return self._encoding_table
-
-    @property
-    def encoder_blocks(self) -> List[BasicModule]:
-        return self._encoder_blocks
-
-    @property
-    def decoder_blocks(self) -> List[BasicModule]:
-        return self._decoder_blocks
-
-    @property
-    def class_number(self) -> int:
-        return self._class_number
-
-    @property
-    def classify_layer(self) -> Weight:
-        return self._classify_layer
-
-    @property
     def normalization(self) -> Normalization:
         return self._normalization
 
-    @property
-    def classifier(self):
-        return self._classifier
-
-    @property
-    def auto_graph(self) -> bool:
-        return self._auto_graph
-
-    def create_mask_matrix(self, input_data: tf.Tensor) -> Tuple[tf.Tensor]:
-        padding_mask = tf.cast(tf.math.equal(input_data[:,:,0], 0), tf.float32)[:, tf.newaxis, tf.newaxis, :]
-        encodding_mask = tf.cast(tf.math.not_equal(input_data[:,:,:], 0), tf.float32)
-        prediction_mask = tf.tile(tf.cast(tf.math.not_equal(input_data[:,:,:1], 0), tf.float32), [1, 1, self.class_number])
-        return padding_mask, encodding_mask, prediction_mask
-
-    def create_encoding_table(self)-> tf.Tensor:
-        basic_table = np.zeros(shape=[self.max_length, self.embedding_size])
-        position = np.arange(self.max_length).reshape([-1, 1])
-        denominator = np.power(10000, np.arange(0, self.embedding_size, 2) / self.embedding_size)
-        basic_table[:, 0::2] = np.sin(position / denominator)
-        basic_table[:, 1::2] = np.cos(position / denominator)
-        return tf.convert_to_tensor(basic_table, dtype=tf.float32)
-
-    def positional_encoding(self, input_data: tf.Tensor, encodding_mask: tf.Tensor) -> tf.Tensor:
-        shape = input_data.shape
-        loolkup_index = tf.tile(tf.range(0, shape[1])[tf.newaxis, :], [shape[0], 1])
-        input_data += tf.nn.embedding_lookup(self.encoding_table, loolkup_index) * encodding_mask
-        return input_data
-
-    def model(self, input_data: tf.Tensor) -> tf.Tensor:
-        padding_mask, encodding_mask, prediction_mask = self.create_mask_matrix(input_data)
-        input_data = self.positional_encoding(input_data, encodding_mask)
+    def model(self, encoder_input: tf.Tensor, decoder_input: tf.Tensor=None) -> tf.Tensor:
+        padding_mask, encoder_encodding_mask, prediction_mask = self.create_mask_matrix(encoder_input)
+        encoder_input = self.positional_encoding(encoder_input, encoder_encodding_mask)
         if self.initial_norm:
-            input_data = self.normalization(input_data)
+            encoder_input = self.normalization(encoder_input)
+        output = encoder_input
         for encoder_block in self.encoder_blocks:
-            output = encoder_block(input_data, padding_mask)
-            input_data = output
-        if self.decoder_blocks is not None:
+            output = encoder_block(output, padding_mask)
+        if self.decoder_blocks != []:
+            padding_mask, decoder_encodding_mask, prediction_mask = self.create_mask_matrix(decoder_input)
+            decoder_input = self.positional_encoding(decoder_input, decoder_encodding_mask)
+            encoder_output = output * encoder_encodding_mask
+            output = decoder_input
             for decoder_block in self.decoder_blocks:
-                output = decoder_block(input_data)
-                input_data = output
+                output = decoder_block(input_data=output, encoder_output=encoder_output, mask=padding_mask)
         prediction = self.classifier(tf.matmul(output, self.classify_layer.value))
         return prediction * prediction_mask
+
+    @classmethod
+    def quick_build(cls,
+                    block_number: int,
+                    head_number: int,
+                    embedding_size :int,
+                    scale_rate: int,
+                    class_number :int,
+                    max_length: int=2000,
+                    normalization: Normalization=None,
+                    initial_norm: bool=False) -> Former:
+        encoder_blocks = []
+        decoder_blocks = []
+        for i in range(block_number):
+            encoder_name = f"encoder_block_{i}"
+            encoder_blocks.append(encoder_block(encoder_name, embedding_size, head_number, scale_rate))
+            decoder_name = f"decoder_block_{i}"
+            decoder_blocks.append((decoder_block(decoder_name, embedding_size, head_number, scale_rate)))
+        return cls(obj_name="Transformer", encoder_blocks=encoder_blocks, class_number=class_number, decoder_blocks=decoder_blocks,
+                   max_length=max_length, normalization=normalization, initial_norm=initial_norm, embedding_size=embedding_size)
