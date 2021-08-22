7d3f2035371d78087deda015f9820fa60dbe7e92
蔡東棋 <awoo.abnertsai@gmail.com>
2021-08-22 19:59:07 +0800

    [NEW] add batch normalization and convolution_layer.py

--- ospark/nn/component/normalization.py
+++ ospark/nn/component/normalization.py
@@ -1,7 +1,35 @@
 import tensorflow as tf
-from typing import NoReturn
+from ospark.nn.component.basic_module import BasicModule
+from typing import NoReturn, List, Optional, Callable
+import ospark
 
-class Normalization:
+class Normalization(BasicModule):
+
+    def __init__(self,
+                 obj_name: str,
+                 gamma_initializer: Optional[float]=None,
+                 beta_initializer: Optional[float]=None,
+                 epsilon: Optional[float]=None):
+        super().__init__(obj_name=obj_name)
+        self._gamma_initializer  = gamma_initializer or 1.
+        self._beta_initializer   = beta_initializer or 0.
+        self._epsilon            = epsilon or 0.0001
+
+    @property
+    def gamma_initializer(self) -> tf.Tensor:
+        return self._gamma_initializer
+
+    @property
+    def beta_initializer(self) -> tf.Tensor:
+        return self._beta_initializer
+
+    @property
+    def epsilon(self) -> tf.Tensor:
+        return tf.constant(self._epsilon)
+
+    def initialize(self) -> NoReturn:
+        self.assign(component=ospark.Weight(obj_name="gamma", initial_value=self.gamma_initializer))
+        self.assign(component=ospark.Weight(obj_name="beta", initial_value=self.beta_initializer))
 
     def __init_subclass__(cls) -> NoReturn:
         super().__init_subclass__()
@@ -22,7 +50,82 @@ class PassNormalization(Normalization):
 
 class LayerNormalization(Normalization):
 
-    def calculate(self, input_data: tf.Tensor) -> tf.Tensor:
-        mean, variance = tf.nn.moments(input_data, axes=[1], keepdims=True)
-        normalization_outputs = (input_data - mean) / tf.sqrt(variance + 1e-5)
-        return normalization_outputs
+    def __init__(self,
+                 obj_name: Optional[str]=None,
+                 gamma_initializer: Optional[float]=None,
+                 beta_initializer: Optional[float]=None,
+                 epsilon: Optional[float]=None):
+        super().__init__(obj_name=obj_name or "layer_norm",
+                         gamma_initializer=gamma_initializer,
+                         beta_initializer=beta_initializer,
+                         Optional=epsilon)
+
+    def calculate(self, input_data: tf.Tensor, axis: int=-1) -> tf.Tensor:
+        mean, variance = tf.nn.moments(input_data, axes=[axis], keepdims=True)
+        normalization_outputs = (input_data - mean) / (tf.sqrt(variance) + self.epsilon)
+        return self.assigned.gamma * normalization_outputs + self.assigned.beta
+
+class BatchNormalization(Normalization):
+
+    def __init__(self,
+                 input_depth: tf.Tensor,
+                 obj_name: str=None,
+                 gamma_initializer: Optional[float]=None,
+                 beta_initializer: Optional[float]=None,
+                 epsilon: Optional[float]=None,
+                 momentum: Optional[float]=None,
+                 moving_mean_initializer: Optional[float]=None,
+                 moving_variance_initializer: Optional[float]=None,
+                 trainable: bool=True):
+        super(BatchNormalization, self).__init__(obj_name=obj_name or "batch_norm",
+                                                 gamma_initializer=gamma_initializer,
+                                                 beta_initializer=beta_initializer,
+                                                 epsilon=epsilon)
+        self._input_depth                 = input_depth
+        self._momentum                    = momentum or 0.9
+        self._moving_mean_initializer     = moving_mean_initializer or 0.
+        self._moving_variance_initializer = moving_variance_initializer or 1.
+        self._calculate                   = self.train_process if trainable else self.infer_process
+
+    @property
+    def input_depth(self) -> tf.Tensor:
+        return self._input_depth
+
+    @property
+    def momentum(self) -> tf.Tensor:
+        return tf.constant(self._momentum)
+
+    @property
+    def moving_mean_initializer(self) -> tf.Tensor:
+        return tf.ones(shape=[self.input_depth]) * self._moving_mean_initializer
+
+    @property
+    def moving_variance_initializer(self) -> tf.Tensor:
+        return tf.ones(shape=[self.input_depth]) * self._moving_variance_initializer
+
+    def calculate(self, input_data: tf.Tensor) -> Callable[[tf.Tensor], tf.Tensor]:
+        return self._calculate(input_data)
+
+    def initialize(self) -> NoReturn:
+        super().initialize()
+        self.assign(component=ospark.Weight(obj_name="moving_mean", initial_value=self.moving_mean_initializer))
+        self.assign(component=ospark.Weight(obj_name="moving_variance", initial_value=self.moving_variance_initializer))
+
+    def train_process(self, input_data: tf.Tensor) -> tf.Tensor:
+        mean, variance = tf.nn.moments(x=input_data, axes=[0, 1, 2])
+        moving_mean = self.assigned.moving_mean * self.momentum + mean * (1 - self.momentum)
+        moving_varience = self.assigned.moving_variance * self.momentum + variance * (1 - self.momentum)
+        return tf.nn.batch_normalization(x=input_data,
+                                         mean=moving_mean,
+                                         variance=moving_varience,
+                                         scale=self.assigned.gamma,
+                                         offset=self.assigned.beta,
+                                         variance_epsilon=self.epsilon)
+
+    def infer_process(self, input_data: tf.Tensor) -> tf.Tensor:
+        return tf.nn.batch_normalization(x=input_data,
+                                         mean=self.assigned.moving_mean,
+                                         variance=self.assigned.moving_variance,
+                                         scale=self.assigned.gamma,
+                                         offset=self.assigned.beta,
+                                         variance_epsilon=self.epsilon)
---
+++ ospark/nn/layer/convolution_layer.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+from ospark.nn.layer import Layer
+from ospark.nn.component.activation import Activation, ReLU
+from ospark.nn.component.normalization import Normalization, BatchNormalization
+from typing import List, NoReturn, Optional
+import tensorflow as tf
+import ospark
+
+
+class ConvolutionLayer(Layer):
+
+    def __init__(self,
+                 obj_name: str,
+                 filter_size: List[int],
+                 strides: List[int],
+                 padding: str,
+                 activation: Activation,
+                 normalization: Normalization,
+                 layer_order: List[str]) -> NoReturn:
+        super().__init__(obj_name=obj_name)
+        self._layer_order   = layer_order
+        self._filter_size   = filter_size
+        self._strides       = strides
+        self._padding       = padding
+        self._activation    = activation
+        self._normalization = normalization
+
+    @property
+    def filter_size(self) -> List[int]:
+        return self._filter_size
+
+    @property
+    def strides(self) -> List[int]:
+        return self._strides
+
+    @property
+    def padding(self) -> str:
+        return self._padding
+
+    @property
+    def activation(self) -> Activation:
+        return self._activation
+
+    @property
+    def normalization(self) -> Normalization:
+        return self._normalization
+
+    @property
+    def layer_order(self) -> List[str]:
+        return self._layer_order
+
+    @classmethod
+    def bn_relu_conv(cls,
+                     obj_name: str,
+                     filter_size: List[int],
+                     strides: List[int],
+                     padding: str,
+                     ) -> ConvolutionLayer:
+        activation = ReLU()
+        normaliztion = BatchNormalization(input_depth=filter_size[-2])
+        return cls(obj_name=obj_name,
+                   filter_size=filter_size,
+                   strides=strides,
+                   padding=padding,
+                   activation=activation,
+                   normalization=normaliztion,
+                   layer_order=["norm", "activate", "conv"])
+
+    @classmethod
+    def conv_relu_bn(cls,
+                     obj_name: str,
+                     filter_size: List[int],
+                     strides: List[int],
+                     padding: str,
+                     ) -> ConvolutionLayer:
+        activation = ReLU()
+        normaliztion = BatchNormalization(input_depth=filter_size[-2])
+        return cls(obj_name=obj_name,
+                   filter_size=filter_size,
+                   strides=strides,
+                   padding=padding,
+                   activation=activation,
+                   normalization=normaliztion,
+                   layer_order=["conv", "activate", "norm"])
+
+    def model(self, input_data: tf.Tensor) -> tf.Tensor:
+        output = input_data
+        for layer_name in self.layer_order:
+            output = self.__dict__[layer_name](output)
+        return output
+
+    def initialize(self) -> NoReturn:
+        self.assign(component=ospark.weight.truncated_normal(obj_name="filter", weight_shape=self.filter_size), name="filter")
+        self.assign(component=self.normalization, name="normalization")
+
+    def conv(self, input_data: tf.Tensor) -> tf.Tensor:
+        output = tf.nn.conv2d(input=input_data, filters=self.assigned.filter, strides=self.strides, padding=self.padding)
+        return output
+
+    def norm(self, input_data: tf.Tensor) -> tf.Tensor:
+        output= self.assigned.normalization(input_data)
+        return output
+
+    def activate(self, input_data: tf.Tensor) -> tf.Tensor:
+        output = self.activation(input_data)
+        return output
+
+
+
