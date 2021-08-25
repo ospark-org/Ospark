[FIX] modify layer normalization and added argument name

ospark/nn/component/normalization.py
@@ -58,7 +58,7 @@ class LayerNormalization(Normalization):
         super().__init__(obj_name=obj_name or "layer_norm",
                          gamma_initializer=gamma_initializer,
                          beta_initializer=beta_initializer,
-                         Optional=epsilon)
+                         epsilon=epsilon)
 
     def calculate(self, input_data: tf.Tensor, axis: int=-1) -> tf.Tensor:
         mean, variance = tf.nn.moments(input_data, axes=[axis], keepdims=True)

ospark/nn/component/weight.py
@@ -43,7 +43,7 @@ class Weight:
 
     def restore(self, weight: tf.Tensor) -> NoReturn:
         if weight is not None:
-            if all(tf.shape(weight) == tf.constant(self.weight_shape)):
+            if all(tf.shape(weight) == tf.shape(self.initial_value)):
                 self._value = tf.Variable(weight)
             else:
                 print(f"Weight \"{self.indexed_name}\", shape does not match the original setting, so use the initialization weight")

ospark/nn/layer/convolution_layer.py
@@ -55,15 +55,15 @@ class ConvolutionLayer(Layer):
                      filter_size: List[int],
                      strides: List[int],
                      padding: str,
-                     ) -> ConvolutionLayer:
+                     trainable: bool) -> ConvolutionLayer:
         activation = ReLU()
-        normaliztion = BatchNormalization(input_depth=filter_size[-2])
+        normalization = BatchNormalization(input_depth=filter_size[-2], trainable=trainable)
         return cls(obj_name=obj_name,
                    filter_size=filter_size,
                    strides=strides,
                    padding=padding,
                    activation=activation,
-                   normalization=normaliztion,
+                   normalization=normalization,
                    layer_order=["norm", "activate", "conv"])
 
     @classmethod
@@ -72,15 +72,15 @@ class ConvolutionLayer(Layer):
                      filter_size: List[int],
                      strides: List[int],
                      padding: str,
-                     ) -> ConvolutionLayer:
+                     trainable: bool) -> ConvolutionLayer:
         activation = ReLU()
-        normaliztion = BatchNormalization(input_depth=filter_size[-2])
+        normalization = BatchNormalization(input_depth=filter_size[-2], trainable=trainable)
         return cls(obj_name=obj_name,
                    filter_size=filter_size,
                    strides=strides,
                    padding=padding,
                    activation=activation,
-                   normalization=normaliztion,
+                   normalization=normalization,
                    layer_order=["conv", "activate", "norm"])
 
     def model(self, input_data: tf.Tensor) -> tf.Tensor:
@@ -91,19 +91,16 @@ class ConvolutionLayer(Layer):
 
     def initialize(self) -> NoReturn:
         self.assign(component=ospark.weight.truncated_normal(obj_name="filter", weight_shape=self.filter_size), name="filter")
-        self.assign(component=self.normalization, name="normalization")
+        self.assign(component=self.normalization, name="norm")
 
     def conv(self, input_data: tf.Tensor) -> tf.Tensor:
         output = tf.nn.conv2d(input=input_data, filters=self.assigned.filter, strides=self.strides, padding=self.padding)
         return output
 
     def norm(self, input_data: tf.Tensor) -> tf.Tensor:
-        output= self.assigned.normalization(input_data)
+        output= self.assigned.norm(input_data)
         return output
 
     def activate(self, input_data: tf.Tensor) -> tf.Tensor:
         output = self.activation(input_data)
         return output
-
-
-

ospark/nn/layer/feed_forward.py
@@ -38,24 +38,25 @@ class FeedForward(Layer):
         return self._normalization
 
     def initialize(self) -> NoReturn:
-        self.assign(ospark.weight.truncated_normal(
+        self.assign(component=ospark.weight.truncated_normal(
                                 obj_name="mapping2high_dimensional", 
                                 weight_shape=[self.embedding_size, self.scale_rate * self.embedding_size]))
-        self.assign(ospark.weight.truncated_normal(
+        self.assign(component=ospark.weight.truncated_normal(
                                 obj_name="mapping2low_dimensional", 
                                 weight_shape=[self.scale_rate * self.embedding_size, self.embedding_size]))
-        self.assign(ospark.weight.truncated_normal(
+        self.assign(component=ospark.weight.truncated_normal(
                                 obj_name="high_dimensional_bias",
                                 weight_shape=[self.scale_rate * self.embedding_size]))
-        self.assign(ospark.weight.truncated_normal(
+        self.assign(component=ospark.weight.truncated_normal(
                                 obj_name="low_dimensional_bias",
                                 weight_shape=[self.embedding_size]))
+        self.assign(component=self.normalization, name="norm")
 
     def model(self, input_data: tf.Tensor) -> tf.Tensor:
         main_output = self.feedforward(input_data)
         residual_output = self.residual_net(input_data)
         added_residual = tf.add(main_output, residual_output)
-        normalization_output = self.normalization(added_residual)
+        normalization_output = self.assigned.norm(added_residual)
         return normalization_output
 
     def feedforward(self, input_data: tf.Tensor) -> tf.Tensor:

ospark/nn/layer/self_attention.py
@@ -53,13 +53,14 @@ class SelfAttention(Layer):
                                 obj_name="output_weights",
                                 weight_shape=[self.embedding_size, self.embedding_size]
         ))
+        self.assign(component=self.normalization, name="norm")
 
     def model(self, input_data: tf.Tensor, mask: Optional[tf.Tensor]=None) -> tf.Tensor:
         Q, K, V = self.QKV_process(input_data)
         main_output = self.attention_layer(Q, K, V, mask)
         residual_output = self.residual_net(input_data)
         added_residual = tf.add(main_output, residual_output)
-        layer_output = self.normalization(added_residual)
+        layer_output = self.assigned.norm(added_residual)
         return layer_output
 
     def QKV_process(self, input_data: tf.Tensor) -> Tuple[tf.Tensor]:
@@ -130,6 +131,7 @@ class EncoderDecoderAttention(SelfAttention):
                                 obj_name="output_weights",
                                 weight_shape=[self.embedding_size, self.embedding_size]
         ))
+        self.assign(component=self.normalization, name="norm")
 
     def QKV_process(self, input_data: tf.Tensor) -> Tuple[tf.Tensor]:
         encoder_output = tf.tile(self.encoder_output[:, tf.newaxis, tf.newaxis, :, :], [1, 2, 4, 1, 1])