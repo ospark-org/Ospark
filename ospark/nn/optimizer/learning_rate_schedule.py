7861286cb6786ee24356faf143ff93dabe9fcbeb
蔡東棋 <donggicai1991@gmail.com>
2023-11-10 10:03:01 +0800

    [UPDATE] Added lambda parameter.

--- ospark/nn/optimizer/learning_rate_schedule.py
+++ ospark/nn/optimizer/learning_rate_schedule.py
@@ -9,12 +9,13 @@ class LearningRateOption(Enum):
 
 class TransformerWarmup(LearningRateSchedule):
 
-    def __init__(self, model_dimension: int, warmup_step: float=4000.):
+    def __init__(self, model_dimension: int, warmup_step: float=4000., lambda_value: float=1.):
         self.model_dimension = tf.cast(model_dimension, dtype=tf.float32)
-        self.warmup_step     = float(warmup_step)
+        self.warmup_step  = float(warmup_step)
+        self.lambda_value = float(lambda_value)
 
     def __call__(self, step):
         step = tf.cast(step, tf.float32)
-        learning_rate = 1. / tf.sqrt(self.model_dimension) * \
+        learning_rate = self.lambda_value / tf.sqrt(self.model_dimension) * \
                         tf.math.minimum(1 / tf.sqrt(step), step * tf.pow(self.warmup_step, -1.5))
         return learning_rate
